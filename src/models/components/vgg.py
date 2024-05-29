1fb06618b94207237ec386dac87f94ae5110a853
lvmlvm <uts.lvm.technology@gmail.com>
2024-05-29 08:47:19 +0000

    removed redundant ResNet, VGG configurations

--- src/models/components/vgg.py
+++ src/models/components/vgg.py
@@ -7,25 +7,46 @@ class VGG(nn.Module):
                  model_name: str="vgg19_bn",
                  weights: str="DEFAULT",
                  output_shape: list=[2130],
-                 unfreeze_depth: int=2):
+                 unfreeze_depth: int=-1):
         super().__init__()
 
+        model_name = model_name.lower()
         self.output_shape = output_shape[0]
 
         if model_name == 'vgg11_bn' or model_name == 'vgg11':
-            cnn = models.vgg11_bn(pretrained = True if "DEFAULT" else weights)
+            self.cnn = models.vgg11_bn(pretrained = True if "DEFAULT" else weights)
         elif model_name == 'vgg13_bn' or model_name == 'vgg13':
-            cnn = models.vgg13_bn(pretrained = True)
+            self.cnn = models.vgg13_bn(pretrained = True)
         elif model_name == 'vgg16_bn' or model_name == 'vgg16':
-            cnn = models.vgg13_bn(pretrained = True)
+            self.cnn = models.vgg16_bn(pretrained = True)
 
-        self.vgg19 = models.vgg19_bn(pretrained=True)
-
-        self.vgg19.classifier[0] = nn.Linear(512 * 2 * 2, 4096)
-        self.vgg19.classifier[6] = nn.Linear(4096, output_shape[0])
+        self.cnn.classifier[0] = nn.Linear(512 * 2 * 2, 4096)
+        self.cnn.classifier[6] = nn.Linear(4096, output_shape[0])
 
     def forward(self, x):
-        x = self.vgg19.features(x)
+        x = self.cnn.features(x)
         x = torch.flatten(x, 1)
-        x = self.vgg19.classifier(x)
+        x = self.cnn.classifier(x)
         return x
+
+if __name__ == "__main__":
+    from torchinfo import summary
+    import torch
+
+    # create a model
+    xla_vgg = VGG("vgg19")
+
+    # show model
+    batch_size = 16
+    summary(model=xla_vgg,
+            input_size=(batch_size, 3, 64, 64),
+            col_names=["input_size", "output_size", "num_params", "trainable"],
+            col_width=20,
+            row_settings=["var_names"])
+    
+    # test input & output shape
+    random_input = torch.randn([16, 3, 64, 64]).to('cpu')
+    xla_vgg.to('cpu')
+    output = xla_vgg(random_input)
+    print(f"\n\nINPUT SHAPE: {random_input.shape}")
+    print(f"OUTPUT SHAPE: {output.shape}")
